9a3c0e70e32225b1142805fa695d76276f33ae97
Eduardo <oeduardoal@gmail.com>
2021-10-20 13:24:32 -0300

    feat: 🎸 passing prop

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Container } from "@grupoboticario/flora-react-container";
+import { Heading } from "@grupoboticario/flora-react-heading";
+import { Button } from "@grupoboticario/flora-react-button";
 
 // @ts-ignore
 import Account from "account/main";
 
 const App: React.FC = () => {
   return (
-    <>
-      Consumer <Account />
-    </>
+    <Container>
+      <Heading level={4}>Header Consumer</Heading>
+      <Button>Consumer Button</Button>
+      {/* MF Account */}
+      <Account name="Eduardo Almeida" />
+    </Container>
   );
 };
 
