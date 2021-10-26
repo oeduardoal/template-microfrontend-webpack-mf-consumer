534f422101a394d04c7fb27cc6d675b09dd2f183
Eduardo Almeida <oeduardoal@gmail.com>
2021-10-26 16:36:52 -0300

    feat: 🎸 script inline

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Recording from "@grupoboticario/flora-screenrecording-addon";
-
-Recording.init({
-  timeout: 5000,
-});
-
 import App from "./app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
